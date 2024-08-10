955bdacd57a8b17d3ab50353f3c378ee1524930f
ZhangZhihui <ZhangZhihuiAAA@126.com>
2024-08-10 19:03:38 +0800

    Simulate the tragedy of the commons.

---
+++ living/commons.py
@@ -0,0 +1,32 @@
+import turtle
+
+
+WIDTH = 1000
+HEIGHT = 800
+
+YEARS = 40
+P = 1000
+r = 0.2
+K = 3000
+HARVEST = 120
+
+
+def setup_screen(title):
+    turtle.setup(WIDTH, HEIGHT)
+    turtle.tracer(0, 0)
+    turtle.title(title)
+    turtle.setworldcoordinates(0, 0, YEARS, K)
+
+
+setup_screen("The tragedy of the commons")
+
+drawer = turtle.Turtle()
+drawer.hideturtle()
+
+for year in range(0, YEARS):
+    drawer.goto(year, P)
+    P += r * P * (1 - P / K)
+    P = max(0, P - HARVEST)
+
+turtle.update()
+turtle.done()
